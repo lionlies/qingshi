57dae083b1548e89277e4eb2bcac7a84b7c6065a
Jason Lung <lionlies@gmail.com>
2016-03-27 19:41:10 +0800

    完成 production 寄信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,13 @@ Rails.application.configure do
   end
 
   config.action_mailer.default_url_options = { host: "https://gentle-beyond-31212.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:           587,
+    address:        "smtp.mailgun.org",
+    user_name:      ENV["mailgun_user"],
+    password:       ENV["mailgun_secret"],
+    domain:         sandbox681a100b7d91471488bab64888bb2129.mailgun.org,
+    authentication: :plain,
+  }
 end

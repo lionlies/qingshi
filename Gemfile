2dd484cf9fb43d24ac560a2c55aa3b961f1d4770
Jason Lung <lionlies@gmail.com>
2016-03-31 16:22:29 +0800

    added facebook login -development function

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem "omniauth-facebook"
 gem "bullet"
 gem "quiet_assets", group: :development
 gem "seo_helper"
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,28 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+  has_many :orders
 
   def admin?
     is_admin
   end
 
   def to_admin
-    self.update_column(is_admin: true)
+    self.update_columns(is_admin: true)
   end
 
   def to_normal
-    self.update_column(is_admin: false)
+    self.update_columns(is_admin: false)
   end
 
-  has_many :orders
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+
 end
